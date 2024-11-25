0bf721f9e9678b0dcacb22869a53a3c5659a9501
Alejandro Dominguez <adborroto90@gmail.com>
2024-11-25 12:21:26 +0100

    chore: bump up homebrew version
    Signed-off-by: Alejandro Dominguez <adborroto90@gmail.com>

--- shellgpt.rb
+++ shellgpt.rb
@@ -1,8 +1,8 @@
 class Shellgpt < Formula
     desc "ShellGPT: Interact with OpenAI from the shell"
     homepage "https://github.com/adborroto/homebrew-shellgpt"
-    url "https://github.com/adborroto/homebrew-shellgpt/archive/v1.0.1.tar.gz" 
-    sha256 "142e3cc9eefca2a17acaeca56443433fc30e2dcb8f7ad0b30406a5484e7bfad9"
+    url "https://github.com/adborroto/homebrew-shellgpt/archive/v1.1.0.tar.gz" 
+    sha256 "c443bff67dcc5e3c32edb4bbb69b4fb1b7e0f321faa8dd3dacd2d4ccea9945c5"
     license "MIT"
   
     depends_on "python@3.10" 
